9df768ddd52cbe723d23d94dcb8a60aabfcca300
JaehyunSong <tintstyle@gmail.com>
2015-04-28 16:36:08 +0900

    no message

--- R/PRcalc.R
+++ R/PRcalc.R
@@ -14,8 +14,7 @@
 #' @description Proportional Representation Calculator for R
 #' @docType package
 #'
-#' @usage PRcalc(nseat, vote, method,
-#'        threshold = 0, viewer = TRUE)
+#' @usage PRcalc(nseat, vote, method, threshold = 0, viewer = TRUE)
 #'
 #' @param nseat Numver of seats(numeric)
 #' @param vote Votes(vector or data.frame)
